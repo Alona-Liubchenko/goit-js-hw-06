bf4295be34c6d6b91dba2f2d8d6e6d056afc0c9b
Alona-Liubchenko <aylin888@rambler.ru>
2022-07-23 00:21:11 +0300

    h3

--- js/task-02.js
+++ js/task-02.js
@@ -8,13 +8,10 @@ const ingredients = [
 ];
 
 const navEl = document.querySelector("#ingredients");
-console.log(navEl);
 
-const navItemEl = document.createElement("li");
-navItemEl.classList.add("item");
-const textEl = ingredients.reduce((value, name) => {
-  value.push(...name);
-}, 0);
-navItemEl.textContent(textEl);
+const elements = navEl.map((element) => {
+  const navItemEl = document.createElement("li");
+  navItemEl.classList.add("item");
+});
 
-navEl.append(...navItemEl);
+navEl.append(navItemEl);
